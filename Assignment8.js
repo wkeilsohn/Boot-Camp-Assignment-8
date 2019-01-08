d7afdd0e0e5e062d913f132f9e1dd839d0c7ab00
William Keilsohn <kingwilliam42@gmail.com>
2019-01-08 13:35:52 -0500

    Central Files
    One html file and a supporting JavaScript file.

---
+++ Assignment8.js
@@ -0,0 +1,9 @@
+var firstname = prompt("What is your first name?");
+var lastname = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Hello "+firstname+" "+lastname+".");
+console.log("Don't you think "+age+" is a little old to be looking at lolis on the internet?");
+
+var timealive = (age*365) + (age/4);
+alert(timealive)
